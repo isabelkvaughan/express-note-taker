ea4d4451e4bd7475a9460655bb8adaff36d4ea8d
Isabel <isabelkvaughan@Isabels-MacBook-Pro.local>
2023-05-05 10:57:48 +1000

    Sample notes added and routes created

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const path = require('path');
+const api = require('./routes/index.js');
 
 // const { clog } = require('./middleware/clog');
-// const api = require('./routes/index.js');
 // Middleware for parsing JSON and urlencoded form data
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 
 // Serve static files from the public directory
@@ -24,7 +24,6 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
 
 // Start the server
 app.listen(PORT, () =>
